35c86daf3e405aeec2d0ddb0a2ffb4e315f40c8f
Rohit Mondal <rohitmondal@Rohits-MacBook-Air.local>
2021-10-19 17:07:04 +0530

    basic functionality

---
+++ src/components/Display/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display=({weatherProperties})=>{
+    console.log(weatherProperties);
+ return (
+     <table>
+         
+     </table>
+ )   
+}
+export default Display
